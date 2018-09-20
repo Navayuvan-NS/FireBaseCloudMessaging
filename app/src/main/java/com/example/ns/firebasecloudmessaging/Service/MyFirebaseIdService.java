b1c946a4a7247113da037c9806b212f59f546e45
Navayuvan-NS <ns.navayuvan@gmail.com>
2018-09-20 13:38:06 +0530

    20.09.2018

---
+++ app/src/main/java/com/example/ns/firebasecloudmessaging/Service/MyFirebaseIdService.java
@@ -0,0 +1,10 @@
+package com.example.ns.firebasecloudmessaging.Service;
+
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseIdService extends FirebaseInstanceIdService {
+
+
+
+}
