861745239f4d34e758b2924eb72f42946f2665bb
Navayuvan-NS <ns.navayuvan@gmail.com>
2018-09-21 19:40:02 +0530

    21.09.2018

--- app/src/main/java/com/example/ns/firebasecloudmessaging/Service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/ns/firebasecloudmessaging/Service/MyFirebaseMessagingService.java
@@ -1,8 +1,15 @@
 package com.example.ns.firebasecloudmessaging.Service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import com.example.ns.firebasecloudmessaging.MainActivity;
+import com.example.ns.firebasecloudmessaging.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+@SuppressWarnings("deprecation")
 public class MyFirebaseMessagingService extends FirebaseMessagingService{
 
     @Override
@@ -12,8 +19,19 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
     }
 
     private void showNotification(RemoteMessage.Notification notification) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
 
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
 
+        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0,builder.build());
 
     }
 }
